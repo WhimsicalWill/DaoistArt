739bfea3741a86ce195497754b49eeda2537c1dc
whimsicalwill <knipewilliam20@gmail.com>
2022-08-07 01:36:55 -0400

    Add basic content to Artwork components

--- src/components/Artwork/Artwork2.jsx
+++ src/components/Artwork/Artwork2.jsx
@@ -8,10 +8,10 @@ const artwork = {
 	name: "Walking on a Mountain Path",
 }
 
-const Artwork2 = ({content}) => {
+const Artwork2 = () => {
 	return  <div className="art2-container">
 				<div className="img2-container">
-					<img src={artwork.image_url} alt="" />
+					<img src="https://localhost:3000/DaoistArt/image-3.jpg" alt="" />
 				</div>
 				<div className="misc2-container">
 					<span className="poem2">{artwork.poem}</span>
