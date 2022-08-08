1f8cd212e788690d6a8a188706c4171b470c4815
whimsicalwill <knipewilliam20@gmail.com>
2022-08-08 12:32:57 -0400

    Revert "Experiment with particles"
    This reverts commit e2533e0f5db70281c5e88d73bd6485aa6aef2395.

--- src/ParticlesBackground.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Particles from "react-particles-js"
-import ParticlesConfig from "./config/particles-config.js"
-
-const ParticlesBackground = () => {
-    return <Particles params={ParticlesConfig}></Particles>
-}
-
-export default ParticlesBackground
