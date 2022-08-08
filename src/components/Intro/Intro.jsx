6941e02b609ff463d2eaed1fadfe2424c70a19a0
whimsicalwill <knipewilliam20@gmail.com>
2022-08-08 16:38:47 -0400

    Finish initial site layout

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,7 +2,7 @@ import './Intro.scss'
 import ParticleBackground from "../../components/Particles/Particles.jsx"
 
 const descriptionPart1 = "Daoism has its roots in 500 B.C. when the philosopher Lao Tzu wrote the Tao Te Ching in ancient China. Daoism emphasizes that humans and animals should live in harmony with the Tao (or Dao), which is effectively the flow of the universe itself. In many ways Daoism overlaps with the traditions of Buddhism and Hinduism, but there are still works that can be considered uniquely Daoist."
-const descriptionPart2 = "In many Daoist works of art, one can see clearly the themes of cosmic balance, otherworldly realms, natural harmony, and man being a part of nature rather than merely in it."
+const descriptionPart2 = "Many Daoist works of art include themes of cosmic balance, otherworldly realms, natural harmony, and man being a part of nature rather than merely in it."
 const daoism_url = "http://localhost:3000/DaoistArt/daoism.svg" 
 
 const Intro = () => {
